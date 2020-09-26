caf333f53787e1769069d46967501f05f8c43622
Julianny Favinha <julianny.favinha@gmail.com>
2020-09-26 15:37:52 -0300

    Initial project

---
+++ MultiplicationForKids/MultiplicationForKids/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationForKids
+//
+//  Created by Julianny Favinha on 26/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplicationForKids/MultiplicationForKids/MultiplicationForKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationForKidsApp.swift
+//  MultiplicationForKids
+//
+//  Created by Julianny Favinha on 26/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationForKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

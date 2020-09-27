0594950b5be8330cea22f6be67499e1e6828daf2
Julianny Favinha <julianny.favinha@gmail.com>
2020-09-27 14:28:20 -0300

    Improve code

--- MultiplicationForKids/MultiplicationForKids/ContentView.swift
+++ MultiplicationForKids/MultiplicationForKids/ContentView.swift
@@ -7,13 +7,21 @@
 
 import SwiftUI
 
+struct Question {
+    let leftValue: Int
+    let rightValue: Int
+
+    var answer: Int {
+        return leftValue * rightValue
+    }
+}
+
 struct ContentView: View {
     @State private var tablesCount = 1
     @State private var questionIndex = 0
-    @State private var questions = ["5", "10", "20"]
+    @State private var questionsCount = ["5", "10", "20"]
 
-    @State private var leftValues: [Int] = []
-    @State private var rightValues: [Int] = []
+    @State private var questions: [Question] = []
 
     @State private var leftValue = 1
     @State private var rightValue = 1
@@ -21,125 +29,106 @@ struct ContentView: View {
 
     @State private var currentQuestion = 0
 
-    @State private var correctAnswers = 0
+    @State private var correctAnswersCount = 0
 
-    @State private var isShowingFinish = false
+    @State private var didFinishGame = false
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 40) {
-            Form {
-                Section(header: Text("How many tables do you want?")) {
-                    Stepper(value: $tablesCount, in: 1...12, step: 1, onEditingChanged: { _ in newGame() }) {
-                        Text("\(tablesCount) tables")
+        Form {
+            Section(header: Text("How many tables do you want?")) {
+                Stepper(value: $tablesCount, in: 1...12, step: 1, onEditingChanged: { _ in newGame() }) {
+                    Text("\(tablesCount) tables")
+                }
+            }
+
+            Section(header: Text("How many questions do you want to answer?")) {
+                Picker("", selection: $questionIndex) {
+                    ForEach(0 ..< questionsCount.count) {
+                        Text("\(self.questionsCount[$0])")
                     }
                 }
+                .pickerStyle(SegmentedPickerStyle())
+            }
 
-                Section(header: Text("How many questions do you want to answer?")) {
-                    Picker("", selection: $questionIndex) {
-                        ForEach(0 ..< questions.count) {
-                            Text("\(self.questions[$0])")
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
+            Section {
+                VStack(alignment: .leading, spacing: 40) {
+                    Text("What is")
 
-                Section {
-                    VStack(alignment: .leading, spacing: 40) {
-                        Text("What is")
-
-                        HStack {
-                            Text(" \(leftValue) ")
-                                .frame(minWidth: 0, maxWidth: .infinity)
-
-                            Text(" X ")
-                                .frame(minWidth: 0, maxWidth: .infinity)
-
-                            Text(" \(rightValue) ")
-                                .frame(minWidth: 0, maxWidth: .infinity)
-
-                            Text(" = ")
-                                .frame(minWidth: 0, maxWidth: .infinity)
-
-                            TextField("", text: $answer)
-                                .frame(minWidth: 0, maxWidth: .infinity)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .keyboardType(.numberPad)
-
-                            Button(action: {
-                                evaluate()
-                            }) {
-                                Text("Test")
-                            }
+                    HStack {
+                        Text(" \(leftValue) ")
                             .frame(minWidth: 0, maxWidth: .infinity)
+
+                        Text(" X ")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+
+                        Text(" \(rightValue) ")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+
+                        Text(" = ")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+
+                        TextField("", text: $answer)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.numberPad)
+
+                        Button(action: {
+                            evaluate()
+                        }) {
+                            Text("Test")
                         }
+                        .frame(minWidth: 0, maxWidth: .infinity)
                     }
                 }
+            }
 
-                Section(header: Text("Correct answers")) {
-                    Text("\(correctAnswers)")
-                }
+            Section(header: Text("Correct answers")) {
+                Text("\(correctAnswersCount)")
             }
         }
         .onAppear(perform: newGame)
-        .alert(isPresented: $isShowingFinish) {
-            Alert(title: Text("Well done!"), message: nil, dismissButton: .default(Text("Continue")) {
+        .alert(isPresented: $didFinishGame) {
+            Alert(title: Text("Well done!"), message: nil, dismissButton: .default(Text("New game")) {
                 self.newGame()
             })
         }
     }
 
     private func newGame() {
-        guard let questionsCount = Int(questions[questionIndex]) else {
+        guard let questionsCount = Int(questionsCount[questionIndex]) else {
             return
         }
 
-        var tables: [Int] = []
-        for index in 1...tablesCount {
-            tables.append(index)
-        }
+        questions = []
 
-        var values: [Int] = []
         for _ in 0..<questionsCount {
-            let randomIndex = Int.random(in: 0..<tablesCount)
-            values.append(tables[randomIndex])
-        }
-        leftValues = values
+            let leftValue = Int.random(in: 1...tablesCount)
+            let rightValue = Int.random(in: 1...10)
 
-        tables = []
-        for index in 1...10 {
-            tables.append(index)
+            questions.append(Question(leftValue: leftValue, rightValue: rightValue))
         }
-
-        values = []
-        for _ in 0..<questionsCount {
-            let randomIndex = Int.random(in: 0..<10)
-            values.append(tables[randomIndex])
-        }
-        rightValues = values
 
         currentQuestion = 0
-        correctAnswers = 0
+        correctAnswersCount = 0
         answer = ""
-        leftValue = leftValues[currentQuestion]
-        rightValue = rightValues[currentQuestion]
+        leftValue = questions[currentQuestion].leftValue
+        rightValue = questions[currentQuestion].rightValue
     }
 
     private func evaluate() {
-        guard let questionsCount = Int(questions[questionIndex]) else {
+        guard let questionsCount = Int(questionsCount[questionIndex]) else {
             return
         }
 
-        let left = Int(leftValue)
-        let right = Int(rightValue)
         let userAnswer = Int(answer)
-        let correctAnswer = left * right
+        let correctAnswer = questions[currentQuestion].answer
 
-        correctAnswers += userAnswer == correctAnswer ? 1 : 0
+        correctAnswersCount += userAnswer == correctAnswer ? 1 : 0
 
         if currentQuestion + 1 < questionsCount {
             nextQuestion()
         } else {
-            isShowingFinish = true
+            didFinishGame = true
         }
     }
 
@@ -147,8 +136,8 @@ struct ContentView: View {
         currentQuestion += 1
         answer = ""
 
-        leftValue = leftValues[currentQuestion]
-        rightValue = rightValues[currentQuestion]
+        leftValue = questions[currentQuestion].leftValue
+        rightValue = questions[currentQuestion].rightValue
     }
 }
 
